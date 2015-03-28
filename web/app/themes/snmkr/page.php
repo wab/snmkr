1c94a34c333fc8ac83d89cc9bc4d14dc3fc12582
wab <contact@wabdesign.fr>
2015-03-29 00:50:36 +0100

    indent & css updates

--- web/app/themes/snmkr/page.php
+++ web/app/themes/snmkr/page.php
@@ -14,23 +14,23 @@
 	}
 
 	$args_menu = array(
-	'authors'      => '',
-	'child_of'     => $parent,
-	'date_format'  => get_option('date_format'),
-	'depth'        => 0,
-	'echo'         => 1,
-	'exclude'      => '',
-	'include'      => '',
-	'link_after'   => '',
-	'link_before'  => '',
-	'post_type'    => 'page',
-	'post_status'  => 'publish',
-	'show_date'    => '',
-	'sort_column'  => 'menu_order, post_title',
-        'sort_order'   => '',
-	'title_li'     => '', 
-	'walker'       => new Nav\Custom_Walker_Page()
-);
+		'authors'      => '',
+		'child_of'     => $parent,
+		'date_format'  => get_option('date_format'),
+		'depth'        => 0,
+		'echo'         => 1,
+		'exclude'      => '',
+		'include'      => '',
+		'link_after'   => '',
+		'link_before'  => '',
+		'post_type'    => 'page',
+		'post_status'  => 'publish',
+		'show_date'    => '',
+		'sort_column'  => 'menu_order, post_title',
+	        'sort_order'   => '',
+		'title_li'     => '', 
+		'walker'       => new Nav\Custom_Walker_Page()
+	);
 
 ?>
 

d7ebb48ab270fd920436c97f65c4d1b3d8edfad7
wab [odev] <wab2com@gmail.com>
2015-05-28 16:24:25 +0200

    actualisation map & modifs disposition page accueil

--- web/app/themes/snmkr/accueil.php
+++ web/app/themes/snmkr/accueil.php
@@ -8,11 +8,11 @@
 
 	$sticky_args = array(
 		'post__in' => $sticky,
-		'posts_per_page' => '3',
+		'posts_per_page' => '1',
 	);
 
 	$actus_args = array(
-		'posts_per_page' => '4',
+		'posts_per_page' => '1',
 		'post__not_in' => $sticky,
 		'tax_query' => array(
 	        array(
@@ -30,7 +30,7 @@
 ?>
 
 
-<?php if ( $sticky_query->have_posts() ) : ?>
+
 
 	<div class="jumbotron">
 
@@ -39,31 +39,35 @@
 
 		<div class="owl-carousel">
 		
-			<!-- the loop -->
-			<?php while ( $sticky_query->have_posts() ) : $sticky_query->the_post(); ?>
-				<div>
-					<?php if (get_post_format() == 'quote') : ?>
-
-						<?php 
-						$attachment_id = 392; // attachment ID
-						$image_attributes = wp_get_attachment_image_src( $attachment_id ); // returns an array
-						if( $image_attributes ) {
-						?> 
-							<img src="<?php echo $image_attributes[0]; ?>" width="<?php echo $image_attributes[1]; ?>" height="<?php echo $image_attributes[2]; ?>" class="pull-right img-thumbnail">
-						<?php } ?>
-					
-					<?php endif; ?>
-					
-					<?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-				</div>
-			<?php endwhile; ?>
-			<!-- end of the loop -->
+			
 
 		</div> <!-- /owl-carousel -->
 
 	<!-- pagination here -->
 
 	</div> <!-- .jumbotron -->
+
+
+<?php if ( $sticky_query->have_posts() ) : ?>
+<!-- the loop -->
+	<?php while ( $sticky_query->have_posts() ) : $sticky_query->the_post(); ?>
+		<div>
+			<?php if (get_post_format() == 'quote') : ?>
+
+				<?php 
+				$attachment_id = 392; // attachment ID
+				$image_attributes = wp_get_attachment_image_src( $attachment_id ); // returns an array
+				if( $image_attributes ) {
+				?> 
+					<img src="<?php echo $image_attributes[0]; ?>" width="<?php echo $image_attributes[1]; ?>" height="<?php echo $image_attributes[2]; ?>" class="pull-right img-thumbnail">
+				<?php } ?>
+			
+			<?php endif; ?>
+			
+			<?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+		</div>
+	<?php endwhile; ?>
+	<!-- end of the loop -->
 <?php endif; ?>
 
 <?php wp_reset_query(); ?>
